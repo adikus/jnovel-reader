9c629028cde956b5b9bf02743c4f6f4a4d94a474
Andrej Hoos <andrej.hoos@gmail.com>
2022-03-13 00:30:40 +0100

    Improve release time difference messages

--- services/date.js
+++ services/date.js
@@ -4,19 +4,21 @@ export default {
     day: 60 * 60 * 24,
     month: 60 * 60 * 24 * 30,
     year: 60 * 60 * 24 * 365,
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
 
     timeDifferenceHumanized(to, from) {
         from = from || new Date();
+        to = new Date(to);
 
         let delta = Math.round((+from - new Date(to)) / 1000);
         if(delta > 0) {
-            return this.timeDifferenceHumanizedInPast(delta);
+            return this.timeDifferenceHumanizedInPast(delta, to, from);
         } else {
-            return this.timeDifferenceHumanizedInFuture(delta * -1);
+            return this.timeDifferenceHumanizedInFuture(delta * -1, to, from);
         }
     },
 
-    timeDifferenceHumanizedInPast(delta) {
+    timeDifferenceHumanizedInPast(delta, to, from) {
         if (delta < 30) {
             return 'just now';
         } else if (delta < this.minute) {
@@ -27,12 +29,12 @@ export default {
             return Math.floor(delta / this.minute) + ' minutes ago';
         } else if (Math.floor(delta / this.hour) === 1) {
             return '1 hour ago'
-        } else if (delta < this.day) {
+        } else if (to > this.endOf(this.yesterday(from))) {
             return Math.floor(delta / this.hour) + ' hours ago';
-        } else if (delta < this.day * 2) {
-            return 'yesterday';
+        } else if (to > this.startOf(this.yesterday(from))) {
+            return 'yesterday ' + to.toLocaleTimeString([], { timeStyle: 'short' });
         } else if (delta < this.month) {
-            return  Math.floor(delta / this.day) + ' days ago';
+            return  Math.ceil(delta / this.day) + ' days ago';
         } else if (Math.floor(delta / this.month) === 1) {
             return '1 month ago';
         } else if (delta < this.year) {
@@ -44,7 +46,7 @@ export default {
         }
     },
 
-    timeDifferenceHumanizedInFuture(delta) {
+    timeDifferenceHumanizedInFuture(delta, to, from) {
         if (delta < 30) {
             return 'just now';
         } else if (delta < this.minute) {
@@ -55,12 +57,14 @@ export default {
             return 'in ' + Math.floor(delta / this.minute) + ' minutes';
         } else if (Math.floor(delta / this.hour) === 1) {
             return 'in 1 hour'
-        } else if (delta < this.day) {
+        } else if (to < this.startOf(this.tomorrow(from))) {
             return 'in ' + Math.floor(delta / this.hour) + ' hours';
-        } else if (delta < this.day * 2) {
-            return 'tomorrow';
+        } else if (to < this.endOf(this.tomorrow(from))) {
+            return 'tomorrow ' + to.toLocaleTimeString([], { timeStyle: 'short' });
+        } else if (delta < this.day * 6) {
+            return 'on ' + this.days[to.getDay()] + ' ' + to.toLocaleTimeString([], { timeStyle: 'short' });
         } else if (delta < this.month) {
-            return  'in ' + Math.floor(delta / this.day) + ' days';
+            return  'in ' + Math.ceil(delta / this.day) + ' days';
         } else if (Math.floor(delta / this.month) === 1) {
             return 'in 1 month';
         } else if (delta < this.year) {
@@ -70,5 +74,31 @@ export default {
         } else {
             return 'in ' + Math.floor(delta / this.year) + ' years';
         }
+    },
+
+    yesterday(from) {
+        let date = new Date(from.getTime());
+        date.setDate(date.getDate() - 1);
+        return date;
+    },
+
+    tomorrow(from) {
+        let date = new Date(from.getTime());
+        date.setDate(date.getDate() + 1);
+        return date;
+    },
+
+    startOf(date) {
+        date.setHours(0);
+        date.setMinutes(0);
+        date.setSeconds(0);
+        return date;
+    },
+
+    endOf(date) {
+        date.setHours(23);
+        date.setMinutes(59);
+        date.setSeconds(59);
+        return date;
     }
 }
